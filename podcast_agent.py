7d4011aab7ea209119bcbcbaae349354c9f957ef
Florian Schäfer <florian.joh.schaefer@gmail.com>
2025-02-07 22:36:46 +0100

    Base podcast agent on latest LightCast version

--- podcast_agent.py
+++ podcast_agent.py
@@ -1,38 +1,21 @@
 import lightcast
 from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
-import lightcast.core
 
 from tool_chain import build_tool_chain
 
 
 @tool
 def find_podcast(search_query: str) -> dict:
-    """
-    Returns the length of a word.
-
-    Args:
-        search_query (str):
-    
-    Returns:
-        dict:
-    
-    """
+    "Returns the length of a word."
     res = lightcast.search_podcasts(search_query)[0]
     return res.__dict__
-
-from urllib.request import urlopen
-from xml.dom import minidom
-from lightcast.core import Episode
 
 
 @tool
 def list_podcast_episodes(feed_url: str) -> dict:
     "List all episode from a podcast given its feed url."
-    xml_str = urlopen(feed_url).read()
-    xmldoc = minidom.parseString(xml_str)
-    episode_items = xmldoc.getElementsByTagName("item")
-    episode_objects = [Episode.from_xml(item) for item in episode_items]
+    episode_objects = lightcast.core.get_episodes_from_feed_url(feed_url)
 
     return [e.__dict__ for e in episode_objects]
 
@@ -42,4 +25,5 @@ model = ChatOllama(model="llama3.2")
 
 chain = build_tool_chain(tools, model)
 print(chain.invoke({"input":"Search for the huberman lab podcast"}))
-print([e["title"] for e in chain.invoke({"input":"List all episodes for podcast https://feeds.megaphone.fm/hubermanlab"})["output"]])
+all_episodes = chain.invoke({"input":"List all episodes for podcast https://feeds.megaphone.fm/hubermanlab"})["output"]
+print([e["title"] for e in all_episodes[:10]])
